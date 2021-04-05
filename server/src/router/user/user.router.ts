dfbadf0cb866784afbfc65a85c275f75d940518c
fesmjke <fatalerror.sm@gmail.com>
2021-04-05 23:45:38 +0300

    removed express, added fastify route options and add their realization

--- server/src/router/user/user.router.ts
+++ server/src/router/user/user.router.ts
@@ -1,13 +1,38 @@
 import Route from "../core/route";
 import { IController } from "../../types/interfaces/controller";
 import { userController } from "../../controllers/user/user.controller";
+import { RouteOptions } from "fastify";
+import { PostUserSchema,PostUserResponce } from "../../types/schema/user";
 
 class UserRoute extends Route {
+    /**
+     Class private fields
+    */
+     private getUsersRoute : RouteOptions;
+     private getUserRoute : RouteOptions;
+     private postUserRoute : RouteOptions;
+     private putUserRoute : RouteOptions;
+     private deleteUserRoute : RouteOptions;
     constructor(rootLabel : string,userController : IController) {
         super(rootLabel,userController);
+        
+        // later need to refactor
+        this.getUsersRoute = { method : "GET",url : `/api/${this._rootLabel}`,handler : this._controller.getAll }
+        this.getUserRoute = { method : "GET",url : `/api/${this._rootLabel}/:id`,handler : this._controller.getById }
+        this.postUserRoute = { method : "POST", 
+            url : `/api/${this._rootLabel}`,
+            schema : {body : PostUserSchema,response : PostUserResponce},
+            handler : this._controller.create}
+        this.postUserRoute
+        this.putUserRoute = { method : "PUT", url : `/api/${this._rootLabel}/:id`, handler : this._controller.updateById }
+        this.deleteUserRoute = { method : "DELETE", url : `/api/${this._rootLabel}/:id`, handler : this._controller.deleteById }
+    }
+
+    public getRouters() : RouteOptions[] {
+        return [this.getUsersRoute,this.getUserRoute,this.postUserRoute,this.putUserRoute,this.deleteUserRoute]
     }
 }
 
 const userRoute = new UserRoute("user",userController);
 
-export default userRoute.compose();
+export default userRoute;
