badd1b4107cd2c088b3e25c6299ebde74b898b25
fesmjke <fatalerror.sm@gmail.com>
2021-04-05 23:43:43 +0300

    removed express and make core router more clear

--- server/src/router/core/route.ts
+++ server/src/router/core/route.ts
@@ -1,25 +1,16 @@
-import { Router } from "express";
+import { RouteOptions } from "fastify";
 import { IController } from "../../types/interfaces/controller";
 
 abstract class Route {
-    private _router : Router;
-    private _controller : IController;
-    private _rootLabel : string;
+    protected _controller : IController;
+    protected _rootLabel : string;
 
     constructor(rootLabel : string,controller : IController) {
         this._rootLabel = rootLabel;
         this._controller = controller;
-        this._router = Router();
     }
 
-    public compose() : Router {
-        this._router.post(`/api/${this._rootLabel}/create`,this._controller.create);
-        this._router.get(`/api/${this._rootLabel}/`,this._controller.getAll);
-        this._router.get(`/api/${this._rootLabel}/:id`,this._controller.getById);
-        this._router.delete(`/api/${this._rootLabel}/:id`,this._controller.deleteById);
-        this._router.put(`/api/${this._rootLabel}/:id`,this._controller.updateById);
-        return this._router;
-    }
+    public abstract getRouters() : RouteOptions[];
 }
 
 export default Route;
