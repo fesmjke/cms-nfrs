b37d3e2ddead670728e63071ddf56b164fdb25aa
fesmjke <fatalerror.sm@gmail.com>
2021-04-05 23:32:18 +0300

    migrated from express to fastify,express full removed.

--- server/src/server/server.ts
+++ server/src/server/server.ts
@@ -1,20 +1,19 @@
-import express from "express";
-import http from "http";
-import { IServerConfiguration } from "../types/interfaces/server-configuration";
+import * as fastify from "fastify";
+import { IServerConfiguration, } from "../types/interfaces/server-configuration";
+import { Server } from "http";
 
 class HttpServer {
-    private _http : http.Server;
     private _configuration : IServerConfiguration;
+    private _application : fastify.FastifyInstance;
 
-    constructor(application : express.Application,serverConfiguration : IServerConfiguration) {
-        this._http = http.createServer(application);
+    constructor(application : fastify.FastifyInstance,serverConfiguration : IServerConfiguration) {
+        this._application = application;
         this._configuration = serverConfiguration;
     }
 
-    public async startServerAsync() : Promise<http.Server> {
-        this._http.listen(this._configuration.port);
-
-        return this._http;
+    public async startServerAsync() : Promise<Server> {
+        this._application.listen({port : this._configuration.port});
+        return this._application.server;
     }
 }
 
