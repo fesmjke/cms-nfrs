c90219530d3d9348adb9c4d331d719ef10bba157
fesmjke <fatalerror.sm@gmail.com>
2021-04-07 20:40:34 +0300

    added db connect

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,18 @@
 import HttpServer from "./server";
 import ApplicationService from "./app/app";
 import UserRouter from "./router/user/user.router";
+import ServerConfigLoader from "./config/server-config.loader";
+import DataBase from "./model/db/db";
 
 const app = new ApplicationService();
 
+DataBase.database_instance.connect()
+
+const serverConfig = new ServerConfigLoader('.config-server.json').loadSync();
+
 app.setRouter(UserRouter.getRouters());
 
-const httpServer = new HttpServer(app.getApp(),{port : 3000});
+const httpServer = new HttpServer(app.getApp(),serverConfig);
 
 async function start() {
     await httpServer.startServerAsync();
