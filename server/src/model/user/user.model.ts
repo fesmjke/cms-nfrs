a5e2bc319ae971b9a791b646d559cd16837c845f
fesmjke <fatalerror.sm@gmail.com>
2021-04-07 20:50:02 +0300

    converted create fucntion to async, and commented part of code

--- server/src/model/user/user.model.ts
+++ server/src/model/user/user.model.ts
@@ -1,47 +1,40 @@
 import { IUser } from "../../types/interfaces/user";
+import { User } from "../db/schema/user.schema";
 
+// rewrite and add new class for user
 class UserModel {
-    // connection;
-    private _users : IUser[];
-
-    constructor(){
-        this._users = [];
+    create = async (user : IUser) : Promise<IUser> => {
+        return await User.create(user);
     }
-
-    create(user : IUser) : IUser {
-        this._users.push(user);
-        user.id = "1";
-        return user;
-    }
-    getAll() : IUser[] {
-        return this._users;
-    }
-    getById(id : string | number) : IUser | object {
-        this._users.forEach(user => {
-            if(user.id === id){
-                return user;
-            }else{
-                return {"id" : id,"status" : "not found"}
-            }
-        });
-        return {"id" : id,"status" : "not found"};
-    }
-    updateById(id : string | number,updatedUser : IUser) : IUser | object {
-        let counter = 0;
-        this._users.forEach(user => {
-            counter +=1;
-            if(user.id === id){
-                this._users[counter] = updatedUser;
-                return updatedUser;
-            }else{
-                return {"id" : id,"status" : "not updated"}
-            }
-        })
-        return {"id" : id,"status" : "not updated"};
-    }
-    deleteById(id : string | number) : object {
-        return {"id" : id,"status" : "not worked"};
-    }
+    // getAll() : IUser[] {
+    //     return this._users;
+    // }
+    // getById(id : string | number) : IUser | object {
+    //     this._users.forEach(user => {
+    //         if(user.id === id){
+    //             return user;
+    //         }else{
+    //             return {"id" : id,"status" : "not found"}
+    //         }
+    //     });
+    //     return {"id" : id,"status" : "not found"};
+    // }
+    // updateById(id : string | number,updatedUser : IUser) : IUser | object {
+    //     let counter = 0;
+    //     this._users.forEach(user => {
+    //         counter +=1;
+    //         if(user.id === id){
+    //             this._users[counter] = updatedUser;
+    //             return updatedUser;
+    //         }else{
+    //             return {"id" : id,"status" : "not updated"}
+    //         }
+    //     })
+    //     return {"id" : id,"status" : "not updated"};
+    // }
+    // deleteById(id : string | number) : object {
+    //     return {"id" : id,"status" : "not worked"};
+    // }
 }
 
 const userModel = new UserModel();
