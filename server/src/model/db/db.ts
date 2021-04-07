21caefe7c216da4df89199b448bb8c28182739a7
fesmjke <fatalerror.sm@gmail.com>
2021-04-07 20:32:53 +0300

    added MongoDB as database

---
+++ server/src/model/db/db.ts
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+import DataBaseConfigLoader from "../../config/db-config.loader";
+import { IDataBaseConfiguration } from "../../types/interfaces/db-configuration";
+
+export default class DataBase {
+    private static _database_instance : DataBase;
+
+    private _connection_url : string;
+    private _connection : mongoose.Connection;
+    private _connection_config : IDataBaseConfiguration;
+    private _db_config_loader : DataBaseConfigLoader;
+
+    constructor(){
+        this._db_config_loader = new DataBaseConfigLoader('.config-database.json');
+        this._connection_config = this._db_config_loader.loadSync();
+        this._connection_url = this._connection_config.host + '/' + this._connection_config.database;
+        this._connection = mongoose.createConnection(this._connection_url,
+                                                    {useCreateIndex : true,useNewUrlParser : true,useUnifiedTopology : true})
+    }
+
+    
+    public get connection() : mongoose.Connection {
+        return this._connection
+    }
+    
+
+    public static get database_instance() : DataBase {
+        return this._database_instance || (this._database_instance = new this());
+    }
+
+    public connect(){
+        mongoose.connect(this._connection_url,{useCreateIndex : true,useNewUrlParser : true,useUnifiedTopology : true},(err) => {
+            if(err){
+                console.log("MONGO ERROR ",err.message)
+            }else{
+                console.log('Connected to MongoDb');
+            }
+        })
+    }
+}
