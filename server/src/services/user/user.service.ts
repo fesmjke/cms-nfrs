51957fb754962008de407977095c98a64bb3d61a
fesmjke <fatalerror.sm@gmail.com>
2021-04-18 15:13:38 +0300

    rewrited user service class

--- server/src/services/user/user.service.ts
+++ server/src/services/user/user.service.ts
@@ -1,6 +1,7 @@
 import {userModel,UserModel} from "../../model/user/user.model";
 import { IUser } from "../../types/interfaces/user";
 import { IService } from "../../types/interfaces/service";
+import { ObjectId } from "mongoose";
 
 class UserService implements IService{
     private _userModel : UserModel;
@@ -12,18 +13,22 @@ class UserService implements IService{
     create = async (user : IUser) : Promise<IUser> => {
         return await this._userModel.create(user);
     }
-    // getAll() : IUser[] {
-    //     return this._userModel.getAll();
-    // }
-    // getById(id : string | number) : IUser | object{
-    //     return this._userModel.getById(id);
-    // }
-    // updateById(id : string | number,updatedUser : IUser) : IUser | object {
-    //     return this._userModel.updateById(id,updatedUser);
-    // }
-    // deleteById(id : string | number) : object {
-    //     return this._userModel.deleteById(id);
-    // }
+    
+    getAll = async () : Promise<IUser[]> => {
+        return await this._userModel.getAll();
+    }
+    
+    getById = async (id : string | number | ObjectId) : Promise<IUser | null> => {
+        return await this._userModel.getById(id);
+    }
+    
+    updateById = async (id : string | number,updatedUser : IUser) : Promise<IUser | null> => {
+        return await this._userModel.updateById(id,updatedUser);
+    }
+
+    deleteById = async (id : string | number | ObjectId) : Promise<IUser | null> => {
+        return await this._userModel.deleteById(id);
+    }
 }
 
 const userService = new UserService(userModel)
