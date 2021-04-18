e25a8c323e4642f7b78e9e394dc75266f83e7c1b
fesmjke <fatalerror.sm@gmail.com>
2021-04-18 15:19:59 +0300

    added pre save function,and password crypting

--- server/src/model/db/schema/user.schema.ts
+++ server/src/model/db/schema/user.schema.ts
@@ -1,21 +1,34 @@
 import { model, Schema } from "mongoose";
 import { IUserDoc } from "../docs/user.doc";
+import bcrypt from "bcrypt";
+
 
 const UserSchema : Schema = new Schema({
-    user_name : { type : String,required : true,unique: true },
+    user_name : { type : String,required : true,unique: [true,'That username is taken.'] },
     name : {type : String,required : true},
     last_name : {type : String,required : true},
-    birth_date : {
-        day : {type : String,required : false},
-        month : {type : String,required : false},
-        year : {type : String,required : false}
-    },
-    phone_number : { type : String,required : false,unique: true },
+    phone_number : { type : String,required : true,unique: [true,'That phone number is taken.'] },
     password : { type : String,required : true},
-    email : { type : String,required : true,unique: true },
+    email : { type : String,required : true,unique: [true,'That email address is taken.'] },
     roles : { type : Array,required : true}
 },{
     timestamps : true,
+}).pre("save", function(this : IUserDoc,next){
+    const user = this;
+    user.roles.push('user')
+    
+    const rounds = 10;
+
+    bcrypt.genSalt(rounds,function(err,salt) {
+        if(err) return next(err);
+        let password = user.password;
+
+        bcrypt.hash(password,salt,function(err,hash){
+            if(err) return next(err);
+            user.password = hash;
+            next();
+        })
+    })
 });
 
 export const User = model<IUserDoc>("User",UserSchema);
