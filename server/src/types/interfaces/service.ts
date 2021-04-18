473a333d64a9eb4ce63c3d751ec4fd4758eda893
fesmjke <fatalerror.sm@gmail.com>
2021-04-18 15:19:01 +0300

    added few changes

--- server/src/types/interfaces/service.ts
+++ server/src/types/interfaces/service.ts
@@ -1,9 +1,10 @@
+import { ObjectId } from "mongoose";
 import { Entity } from "./entity";
 
 export interface IService{
     create(entity : Entity) : Promise<Entity>;
-    // getAll() : Entity[];
-    // getById(id : string | number) : Entity | object;
-    // updateById(id : string | number, updatedEntity : Entity) : Entity | object;
-    // deleteById(id : string | number) : object;
+    getAll() : Promise<Entity[]>;
+    getById(id : string | number | ObjectId) : Promise<Entity | null>;
+    updateById(id : string | number | ObjectId, updatedEntity : object) : Promise<Entity | null>;
+    deleteById(id : string | number | ObjectId) : Promise<object | null>;
 } 
