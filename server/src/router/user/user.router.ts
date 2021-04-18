3ac9170c025deb77d5d66a1bcff025fdddbd733f
fesmjke <fatalerror.sm@gmail.com>
2021-04-18 15:20:42 +0300

    added responce schemas to routes

--- server/src/router/user/user.router.ts
+++ server/src/router/user/user.router.ts
@@ -2,7 +2,7 @@ import Route from "../core/route";
 import { IController } from "../../types/interfaces/controller";
 import { userController } from "../../controllers/user/user.controller";
 import { RouteOptions } from "fastify";
-import { PostUserSchema,PostUserResponce } from "../../types/schema/user";
+import { PostUserSchema,PostUserResponce,GetUserResponce,GetUsersResponce, DeleteUserResponce, UpdateUserResponce } from "../../types/schema/user";
 
 class UserRoute extends Route {
     /**
@@ -17,15 +17,15 @@ class UserRoute extends Route {
         super(rootLabel,userController);
         
         // later need to refactor
-        this.getUsersRoute = { method : "GET",url : `/api/${this._rootLabel}`,handler : this._controller.getAll }
-        this.getUserRoute = { method : "GET",url : `/api/${this._rootLabel}/:id`,handler : this._controller.getById }
+        this.getUsersRoute = { method : "GET",url : `/api/${this._rootLabel}`,schema : {response : GetUsersResponce},handler : this._controller.getAll }
+        this.getUserRoute = { method : "GET",url : `/api/${this._rootLabel}/:id`,schema : {response : GetUserResponce},handler : this._controller.getById }
         this.postUserRoute = { method : "POST", 
             url : `/api/${this._rootLabel}`,
             schema : {body : PostUserSchema,response : PostUserResponce},
             handler : this._controller.create}
         this.postUserRoute
-        this.putUserRoute = { method : "PUT", url : `/api/${this._rootLabel}/:id`, handler : this._controller.updateById }
-        this.deleteUserRoute = { method : "DELETE", url : `/api/${this._rootLabel}/:id`, handler : this._controller.deleteById }
+        this.putUserRoute = { method : "PUT", url : `/api/${this._rootLabel}/:id`,schema : {response : UpdateUserResponce}, handler : this._controller.updateById }
+        this.deleteUserRoute = { method : "DELETE", url : `/api/${this._rootLabel}/:id`,schema : {response : DeleteUserResponce}, handler : this._controller.deleteById }
     }
 
     public getRouters() : RouteOptions[] {
