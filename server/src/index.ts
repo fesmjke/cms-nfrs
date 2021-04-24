6c94626f25d0ce17e939de001dabd3497108aaf1
fesmjke <fatalerror.sm@gmail.com>
2021-04-24 17:12:23 +0300

    added auth routes to router

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,12 @@
 import HttpServer from "./server";
 import ApplicationService from "./app/app";
 import UserRouter from "./router/user/user.router";
+import AuthRouter from "./router/auth/auth.router";
 import ServerConfigLoader from "./config/server-config.loader";
 import DataBase from "./model/db/db";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = new ApplicationService();
 
@@ -11,6 +15,7 @@ DataBase.database_instance.connect()
 const serverConfig = new ServerConfigLoader('.config-server.json').loadSync();
 
 app.setRouter(UserRouter.getRouters());
+app.setRouter(AuthRouter.getRouters());
 
 const httpServer = new HttpServer(app.getApp(),serverConfig);
 
