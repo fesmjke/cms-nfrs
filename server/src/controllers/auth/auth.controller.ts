e6913b711662c0f288e9373016c710a61b52d997
fesmjke <fatalerror.sm@gmail.com>
2021-04-24 17:07:18 +0300

    added auth controller

---
+++ server/src/controllers/auth/auth.controller.ts
@@ -0,0 +1,28 @@
+import { FastifyReply } from "fastify";
+import { IAuthController } from "../../types/interfaces/auth-controller";
+import { IAuthService } from "../../types/interfaces/auth-service";
+import { AuthLogInRequest,AuthLogOutRequest } from "../../types/requests/auth";
+import { authService } from "../../services/auth/auth.service";
+
+class AuthController implements IAuthController {
+    private _authService : IAuthService;
+    
+    constructor(authService : IAuthService) {
+        this._authService = authService;
+    }
+
+    logIn = async (request : AuthLogInRequest,reply : FastifyReply) => {
+        return await this._authService.logIn({email : request.body.email,password : request.body.password}); // rewrite all cases;
+    }
+
+    logOut = async (request : AuthLogOutRequest,reply : FastifyReply) => {
+        return await this._authService.logOut({authId : request.body.authId}); // rewrite all cases;
+    }
+}
+
+const authController = new AuthController(authService);
+
+export {
+    authController,
+    AuthController
+};
