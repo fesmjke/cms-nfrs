f5a9f7219bc9087dac1bc50551930b1828fa141a
fesmjke <fatalerror.sm@gmail.com>
2021-04-25 19:04:02 +0300

    changed method signature,and rewrite implementation

--- server/src/types/interfaces/auth-service.ts
+++ server/src/types/interfaces/auth-service.ts
@@ -1,6 +1,7 @@
+import { FastifyReply } from "fastify";
 import { ILogIn,ILogOut,IAuth } from "./auth";
 
 export interface IAuthService{
-    logIn(logIn : ILogIn) : Promise<IAuth | null | object>;
-    logOut(logOut : ILogOut) : Promise<IAuth | null>;
+    logIn(logIn : ILogIn,reply : FastifyReply) : Promise<IAuth | null | object>;
+    logOut(logOut : ILogOut,reply : FastifyReply) : Promise<IAuth | null>;
 }
