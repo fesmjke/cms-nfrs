87d7ceaf02a9937ed00d43196d1e81de11ff96f1
fesmjke <fatalerror.sm@gmail.com>
2021-04-25 19:04:32 +0300

    added responces and schemas to requests

--- server/src/types/schema/auth.ts
+++ server/src/types/schema/auth.ts
@@ -1,3 +1,6 @@
+import Joi from "joi"
+import { ILogIn, ILogOut } from "../interfaces/auth"
+
 export const AuthLogInResponce = {
 	200 : {
 		type: 'object',
@@ -6,10 +9,43 @@ export const AuthLogInResponce = {
             token : {type : "string"}
         }
 	},
-    400 : {
+    401 : {
         type: 'object',
         properties : {
+            error : {type : "string"},
+            code : {type : "string"},
             message : {type : "string"}
         }
     }
 }
+
+export const AuthLogOutResponce = {
+    200 : {
+		type: 'object',
+        properties: {
+            error : {type : 'string'},
+            result : {type : 'object',
+            properties : {
+                _id : {type : "string"}
+            }},
+            message : {type : "string"}
+        }
+	},
+    401 : {
+        type : 'object',
+        properties : {
+            error : {type : "string"},
+            code : {type : "string"},
+            message : {type : "string"}
+        }
+    }
+}
+
+export const PostLogInSchema = Joi.object<ILogIn>({
+    email : Joi.string().min(3).max(30).required(),
+    password : Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required()
+})
+
+export const PostLogOutSchema = Joi.object<ILogOut>({
+    authId : Joi.string().required()    
+})
