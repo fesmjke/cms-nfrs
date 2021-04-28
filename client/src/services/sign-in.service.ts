4027020166fceb5b858f5baaaebe657f5c1c0340
fesmjke <fatalerror.sm@gmail.com>
2021-04-28 19:47:41 +0300

    added realization for sign-in service

--- client/src/services/sign-in.service.ts
+++ client/src/services/sign-in.service.ts
@@ -1,5 +1,22 @@
-import {  } from "module";
+import {ISignIn} from "../types/interfaces/services";
 
-class SignInService{
-    
+export default class SignInService implements ISignIn {
+    logIn = async () => {
+        const requestOptions = {
+            method : "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body : JSON.stringify({
+                "email" : "qweasdzcc123@example.com",
+	            "password" : "213"
+            })
+        }
+        try {
+            const result = await fetch('auth/login',requestOptions);
+            console.log(await result.json());
+        }catch(err){
+            console.log(err)
+        }
+    }
 }
